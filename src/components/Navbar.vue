2d6912a17165b5237a71d83128407fd7e950b0b9
Sanjeeb <Sanjeeb.lama004@gmail.com>
2019-10-17 15:08:19 +0545

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="hidden-lg-and-up">
+    <v-toolbar flat app class="grey darken-1">
+      <v-app-bar-nav-icon class="white--text" @click.stop="drawer =! drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">
+        <span class="font-weight-light black--text">Solve</span>
+        <span>Vue</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>
+        Help
+        <v-icon right>mdi-help</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer absolute v-model="drawer" class="white lighten-4">
+      <v-list class="my-5 mx-5" v-for="list in lists" :key="list.title">
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon medium class="blue--text" left>mdi-{{list.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{list.title}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      lists: [
+        { title: "Message", icon: "email" },
+        { title: "About", icon: "help" },
+        { title: "Song of the day", icon: "music" },
+        { title: "Real Story", icon: "message" }
+      ],
+      drawer: false
+    };
+  }
+};
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-container class="my-5 mx-5">
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <h1 class="display-4">
+            You have a message from Stranger.
+            <v-btn>
+              <v-icon large>mdi-email</v-icon>
+            </v-btn>
+          </h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-parallax height="300" src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+        <v-row align="center" justify="center">
+          <h1 class="display-2 font-weight-thin mb-4">About WeAreOne</h1>
+          <h4
+            class="subheading"
+          >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, officia, nobis fuga fugiat quaerat, possimus consequatur enim odit quasi iure qui sapiente dicta! Ipsum animi perspiciatis porro alias illo consectetur.</h4>
+        </v-row>
+      </v-parallax>
+    </v-container>
+    <v-card-text class="grey lighten-2">
+      <v-form>
+        <h1 class="display-4">Send Message</h1>
+        <v-text-field label="Name" required prepend-icon="mdi-account-circle" />
+        <v-text-field
+          type="password"
+          label="Password"
+          required
+          prepend-icon="mdi-lock"
+          append-icon="mdi-eye-off"
+        />
+        <v-card-action>
+          <v-btn color="success">Submit</v-btn>
+        </v-card-action>
+      </v-form>
+    </v-card-text>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "",
+  components: {}
+};
+</script>
